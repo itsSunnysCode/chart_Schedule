c3e43a6e149f44dced0610c8bdf3d01c16537ec0
Himanshu Sain <sainhimanshu9@gmail.com>
2019-10-15 21:54:00 +0530

    first commit done

---
+++ src/Components/Assign.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Assign extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TotalCloud Incorporation Assignment</h1>
+            </div>
+        )
+    }
+}
+
+export default Assign
