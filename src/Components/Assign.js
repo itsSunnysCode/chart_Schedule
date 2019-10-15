058ad8bf342f49eaaa1adb531d00b937d4dad3d2
Himanshu Sain <sainhimanshu9@gmail.com>
2019-10-16 00:14:54 +0530

    table created

--- src/Components/Assign.js
+++ src/Components/Assign.js
@@ -8,29 +8,63 @@ class Assign extends Component {
         fetch(proxyURL+url)
         .then((res)=>res.json())
         .then((data)=>{
-            this.setState({dataFromAPI:data});
+            this.setState({students:data});
         }
         )
         .catch((error)=> console.log('there was some problems in fetching some data')
         )
 
     }
+    Table() {
+        return this.state.students.map((student) => {
+           const { id, name, start, end } = student //destructuring
+           let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            let dateNumber = start.substring(0,2);
+            let  temp = start.substring(4,5);
+            let month = monthNames[temp-1];
+            let startDate = `${dateNumber}/${month}`;
+            let end_dateNumber = end.substring(0,2);
+            let  end_temp = end.substring(4,5);
+            let end_month = monthNames[end_temp-1];
+            let endDate = `${end_dateNumber}/${end_month}`;
+
+           return (
+              <tr key={id}>
+                 <td>{id-1}</td>
+                 <td>{name}</td>
+                 <td>{startDate}</td>
+                 <td>{endDate}</td> 
+              </tr>
+           )
+        })
+     }
     constructor(props){
         super(props);
         this.state = {
-            dataFromAPI:[],
+            students:[],
             startDate: '',
             dueDate:''
         };
+
+        this.Table = this.Table.bind(this);
     }
     
     render() {
         return (
             <div>
-                <h1>TotalCloud Incorporation Assignment</h1>
-                { this.state.dataFromAPI.map((singleData)=>{
-                    return <p key={singleData.id}>{singleData.name} {singleData.start} {singleData.end}</p>
-                })}
+            <h1 id='title'>Totalcloud Assignment</h1>
+            <table id='students'>
+                    <tr>
+                    <th></th>
+                    <th>intern-Assignment</th>
+                    <th>start date</th>
+                    <th>due date</th>
+                    </tr>
+               <tbody>
+                  {this.Table()}
+               </tbody>
+            </table>
                
             </div>
         )
